cf64a5d5903c6fd9e8d9f2a6f9f84ef4593c412e
Zahin Zulkipli <zahin@Zahins-MacBook-Pro.local>
2021-10-26 16:39:09 +0800

    change database

--- mysql-db.php
+++ mysql-db.php
@@ -2,14 +2,14 @@
 $servername = "localhost";
 
 // digitalocean database
-// $username = "zahin";
-// $password = "zahinpassword";
-// $dbname = "day_10";
+$username = "zahin";
+$password = "zahinpassword";
+$dbname = "day_10";
 
 // local database
-$username = "root";
-$password = "root";
-$dbname = "day_10";
+// $username = "root";
+// $password = "root";
+// $dbname = "day_10";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
